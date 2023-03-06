0c1806c7ca39c2c1d89568d5f270f55d08c019e4
Hypekage <jmestmnag@gmail.com>
2023-03-06 10:02:04 +0545

    otp sent while registering

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,18 +1,68 @@
 const userModel = require("../Model/userModel");
-const getUser = (req, res) => {
-  res.status(200).json({
-    name: "hah",
-  });
-};
+const nodemailer = require("nodemailer");
 
-const createUser = async (req, res) => {
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: "587",
+  auth: {
+    user: "codewithsaroj@gmail.com",
+    pass: "ijjcsivjbpyjwbwz",
+  },
+  secureConnection: "false",
+  tls: {
+    ciphers: "SSLv3",
+    rejectUnauthorized: false,
+  },
+});
+
+const registerUser = async (req, res,next) => {
+  const { userName, email, phone, password, confirm_password } = req.body;
+  const otp = Math.floor(100000 + Math.random() * 900000);
   try {
-    const user = await userModel.create({ ...req.body });
-    res.status(200).send("ok");
+    if (!userName || !email || !phone || !password || !confirm_password) {
+      res.status(412).json({
+        message: "Empty Field Detected",
+      });
+    } else if (password !== confirm_password) {
+      res.status(412).json({
+        message: "Given Password did not match",
+      });
+    } else {
+      const mailOptions = {
+        from: "codewithsaroj@gmail.com",
+        to: `${email}`,
+        subject: "OTP Code for Registration",
+        text: `Your OTP code is ${otp}.`,
+      };
+
+      await transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+          req.userOtp = {
+            otp: otp
+          }
+          console.log(req.userOtp)
+          next()
+          // res.status(200).json({
+          //   message: "OTP has been sent to your email.",
+          // });
+        }
+      });
+      
+    }
+    // const user = await userModel.create({ ...req.body });
   } catch (err) {
     console.log(err);
   }
 };
+
+const verifyOtp = async (req,res) => {
+  console.log(req.userOtp);
+  res.send("ok");
+}
 
 const loginUser = async (req, res) => {
   try {
@@ -29,4 +79,4 @@ const loginUser = async (req, res) => {
   }
 };
 
-module.exports = { getUser, createUser, loginUser };
+module.exports = { registerUser, loginUser ,verifyOtp};
