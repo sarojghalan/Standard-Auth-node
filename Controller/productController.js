57b1965acc6e3ead73c05bfca3c12ec90c9c5a2d
Hypekage <jmestmnag@gmail.com>
2023-03-14 18:16:23 +0545

    product single fetched and updated successfully

--- Controller/productController.js
+++ Controller/productController.js
@@ -2,39 +2,58 @@ const asyncHandler = require("express-async-handler");
 const productSchema = require("../Model/productModel");
 const getProducts = async (req, res) => {
   const products = await productSchema.find();
-  res.status(200).json({
-    message:"all products fetched successfully",
-    status:res.statusCode,
-    data:products
+  return res.status(200).json({
+    message: "all products fetched successfully",
+    status: res.statusCode,
+    data: products,
   });
 };
-const getProduct = asyncHandler( async (req, res) => {
+const getProduct = async (req, res) => {
   console.log(req.params.id);
-  const singleProduct = await productSchema.findOne({_id:req.params.id})
-  if(singleProduct === null){
-    res.status(412).json({
-      message:"Product of that id is not available.",
-      status:res.statusCode
-    })
+  const singleProduct = await productSchema.findOne({ _id: req.params.id });
+
+  if (singleProduct === null) {
+    console.log("i am being called");
+    return res.status(412).json({
+      message: "Product of that id is not available.",
+      status: res.statusCode,
+    });
   }
+  console.log("i am 2nd called");
   res.status(200).json({
-    message:"Data fetched successfully",
-    status:res.statusCode,
-    data:singleProduct
-  })
-});
+    message: "Data fetched successfully",
+    status: res.statusCode,
+    data: singleProduct,
+  });
+};
 const createProduct = asyncHandler(async (req, res) => {
-  const {_id, productName, price, brand, description } = req.body;
-  const productChecker = await productSchema.findOne({_id:_id})
+  const { _id, productName, price, brand, description } = req.body;
+  const productChecker = await productSchema.findOne({ _id: _id });
   const product = await productSchema.create({ ...req.body });
   console.log(productChecker);
-  if(product !== null){
-    res.status(400).json({ message: "Product of this id has been already created" });
+  if (product !== null) {
+    res
+      .status(400)
+      .json({ message: "Product of this id has been already created" });
   }
   res.status(200).json({ message: "Product created successfully" });
 });
-const updateProduct = (req, res) => {
-  res.send("update products");
+const updateProduct = async (req, res) => {
+  const updateProduct = await productSchema.findByIdAndUpdate(req.params.id, { ...req.body });
+  if (!updateProduct) {
+    return res.status(404).json({
+      message: "Product of that id has not found.",
+    });
+  }
+  const productInfo = await productSchema.findOne({_id:req.params.id});
+  console.log("product info : ",productInfo)
+  res.status(200).json({
+    message: "Product has been updated successfully",
+    status: res.statusCode,
+    data: {
+      productInfo,
+    },
+  });
 };
 const deleteProduct = (req, res) => {
   res.send("delete products");
